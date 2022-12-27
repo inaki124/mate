4d0ecc0d8e90a024d6dafb429e7f806cc2120790
inaki124 <113951615+inaki124@users.noreply.github.com>
2022-12-26 20:26:13 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function addUser() {
+  
+    player1_name=document.getElementById("parangaricutirimicuaro1").value;
+    player2_name=document.getElementById("parangaricutirimicuaro2").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+
+    window.location="game_page.html";
+
+}function addUser() {
+ 
+    player1_name=document.getElementById("parangaricutirimicuaro1").value;
+    player2_name=document.getElementById("parangaricutirimicuaro2").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+
+    window.location="game_page.html";
+
+}   
