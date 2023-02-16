b90256256066f9f59730d709db00846b585a354c
inaki124 <113951615+inaki124@users.noreply.github.com>
2023-02-16 17:16:22 -0600

    Delete quiz_game_page.js

--- quiz_game_page.js
+++
@@ -1,5 +0,0 @@
-function send() {
-    number1=document.getElementById("number1").value;
-    number2=document.getElementById("number2").value;
-    actual_answer=parseInt(number1)*parseInt(number2);
-}
